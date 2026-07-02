9b522a0b3e783569b2ea2b73667a0e60ddf8570e
MaciejSchck <maciejschck@gmail.com>
2026-07-02 14:41:02 +0200

    Create Kasztan.java

---
+++ SklepBack/Kasztan.java
@@ -0,0 +1,17 @@
+package com.kasztany.sklep;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Kasztan {
+    public int id;
+    public String shape;
+    public String size;
+    public String color;
+    public int shankLenght;
+    public double price;
+    public String photoURL;
+
+
+
+}
